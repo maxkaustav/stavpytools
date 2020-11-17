b0f76b99bf0082b01098af9adc0249421856468a
Kaustav <kaustav28c@gmail.com>
2020-11-17 22:32:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+REQUIREMENTS = [
+                'numpy',
+                'pandas',
+                'matplotlib',
+                'seaborn',
+                'tensorflow >= 2.2'
+                ]
+setuptools.setup(
+    name="stavpytools", # Replace with your own username
+    version="1.0-rc",
+    author="Kaustav Chaudhury",
+    author_email="kaustav28c@gmail.com",
+    description="This helps to reduce code by predefined packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxkaustav/stavpytools",
+    install_requires=REQUIREMENTS,
+    python_requires='>=3.6',
+)
